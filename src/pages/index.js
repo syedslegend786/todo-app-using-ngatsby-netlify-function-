7fec9d3233e27704ec2d7d7059c4a3be088eab3b
Syed Salleh Shah <syedslegend786@gmail.com>
2021-10-05 00:09:31 -0700

    add the netlify identity widget

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,15 @@
 import * as React from "react"
-
+import netlifyidentity from 'netlify-identity-widget'
 const IndexPage = () => {
+  React.useEffect(() => {
+    netlifyidentity.init({})
+  })
   return (
-    <div>hello index...</div>
+    <div>
+      <h1>gatsby todo app</h1>
+      <button onClick={() => netlifyidentity.open()}>login</button>
+      <button onClick={() => console.log(netlifyidentity.currentUser())}>current user</button>
+    </div>
   )
 }
 
